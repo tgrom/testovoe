21d9dbe5c58d42a5a46559e4e5c48beb554c8422
Romanchenko Tatyana <romanchenko_t@bsu.edu.ru>
2022-10-17 20:19:57 +0300

    Генерация логина и пароля

--- app/Imports/StudentsImport.php
+++ app/Imports/StudentsImport.php
@@ -4,9 +4,25 @@ namespace App\Imports;
 
 use App\Models\Student;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithValidation;
 
-class StudentsImport implements ToModel
+
+class StudentsImport implements ToModel, WithValidation
 {
+    public function rules(): array
+    {
+        return [
+            '2' => 'unique:students,email',
+            '5' => 'unique:students,login'
+        ];
+    }
+    public function customValidationAttributes()
+    {
+        return ['2' => 'E-mail',
+                '5' => 'Логин'];
+    }
+
+
     /**
     * @param array $row
     *
@@ -15,13 +31,13 @@ class StudentsImport implements ToModel
     public function model(array $row)
     {
         return new Student([
-            'user_family'     => $row[1],
-            'user_name'       => $row[2],
-            'email'           => $row[3],
-            'country'         => $row[4],
-            'city'            => $row[5],
-            'login'           => $row[6],
-            'pass'            => $row[7],
+            'user_family'     => $row[0],
+            'user_name'       => $row[1],
+            'email'           => $row[2],
+            'country'         => $row[3],
+            'city'            => $row[4],
+            'login'           => $row[5],
+            'pass'            => $row[6],
         ]);
     }
 }
